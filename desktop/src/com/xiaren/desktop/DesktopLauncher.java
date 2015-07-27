2947374be6b3e66ab961ece39cc0ac34061f0ab4
Easterling <blackboy2142@live.com>
2015-07-27 08:01:35 -0400

    Initial commit. Going to dive straight in to rendering.

---
+++ desktop/src/com/xiaren/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.xiaren.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.xiaren.XiaRen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XiaRen(), config);
+	}
+}
