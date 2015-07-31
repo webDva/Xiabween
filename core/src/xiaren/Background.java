5e12f80b66e79107a4ef98a893cc2127fc30515a
Easterling <blackboy2142@live.com>
2015-07-31 00:29:31 -0400

    Straightened the Background class.

--- core/src/xiaren/Background.java
+++ core/src/xiaren/Background.java
@@ -7,19 +7,24 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 
 public class Background {
 
-	private String backgroundimage;
+	private final String backgroundimage;
 
-	public FileHandle Background(String imagetoload) {
+	public Background(String imagetoload) { // imagetoload is the file path of the image to load.
 
-		// This constructor will load a file into memory, so that the
+		this.backgroundimage = imagetoload;
+
+	}
+
+	public FileHandle loadFIle() {
+
+		// This method will load a file into memory, so that the
 		// class can read the file's entire contents, bit by bit, as
 		// in assembly language parsing data structures and file formats.
 
-		this.backgroundimage = imagetoload;
 		FileHandle fhimage = null;
 
 		try {
-			fhimage = Gdx.files.getFileHandle(imagetoload, Files.FileType.Internal);
+			fhimage = Gdx.files.getFileHandle(this.backgroundimage, Files.FileType.Internal);
 			return fhimage;
 		} catch (GdxRuntimeException e) {
 			System.err.println(e);
