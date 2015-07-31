17b1bf2042580dee76569923e080f601fc4242fc
Easterling <blackboy2142@live.com>
2015-07-30 21:40:01 -0400

    Removed the com.package naming.

---
+++ core/src/xiaren/XiaRen.java
@@ -0,0 +1,33 @@
+// Forget about what to render right now, and just start rendering.
+
+package xiaren;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class XiaRen extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		// First, render the background, then center camera on the player's character.
+		// So that means to create a player object and a camera object as well as a
+		// background object. Or class.
+
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+}
---
+++ desktop/src/xiaren/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package xiaren.desktop;
+
+import xiaren.XiaRen;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XiaRen(), config);
+	}
+}
