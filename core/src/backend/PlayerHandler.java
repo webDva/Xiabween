96e7cec5f3be46995059da01a116a155bad4d938
sorcerer <windows146@users.noreply.github.com>
2015-08-17 12:37:19 -0400

    PlayerHandler.movePlayer now changes player.direction.

--- core/src/backend/PlayerHandler.java
+++ core/src/backend/PlayerHandler.java
@@ -8,7 +8,22 @@ public class PlayerHandler {
 
 	public static void movePlayer(PlayerCharacter player, Vector2 coordinates) {
 		player.position = coordinates;
+		float x_delta, y_delta; // Should really learn about variable naming conventions. Felt the underscore one here
+								// was necessary, because of their type and the x.
+		x_delta = coordinates.x - player.position.x;
+		y_delta = coordinates.y - player.position.y;
 
+		if (x_delta < 0) {
+			player.direction = "left";
+		} else if (x_delta > 0) {
+			player.direction = "right";
+		}
+
+		if (y_delta < 0) {
+			player.direction = "down";
+		} else if (y_delta > 0) {
+			player.direction = "up";
+		}
 	}
 
 	public static void changeHealth(PlayerCharacter player, float health) {
