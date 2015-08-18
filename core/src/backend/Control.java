d29f0bc0d1ddaad24a0a999e55fe0b46315c25ce
sorcerer <windows146@users.noreply.github.com>
2015-08-18 18:06:02 -0400

    Successfully changed checkIfMovementKeyIsPressed to use displacements.

--- core/src/backend/Control.java
+++ core/src/backend/Control.java
@@ -12,31 +12,31 @@ public class Control { // Handles input from the player user. This goes into pro
 
 	public static void checkIfMovementKeyIsPressed(PlayerCharacter player) {
 		if (Gdx.input.isKeyPressed(Keys.W) && Gdx.input.isKeyPressed(Keys.A)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x - 1, player.position.y + 1));
+			PlayerHandler.movePlayer(player, new Vector2(-1, 1));
 		}
 		if (Gdx.input.isKeyPressed(Keys.W) && Gdx.input.isKeyPressed(Keys.D)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x + 1, player.position.y + 1));
+			PlayerHandler.movePlayer(player, new Vector2(1, 1));
 		}
 		if (Gdx.input.isKeyPressed(Keys.W)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x, player.position.y + 1));
+			PlayerHandler.movePlayer(player, new Vector2(0, 1));
 		}
 
 		if (Gdx.input.isKeyPressed(Keys.A)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x - 1, player.position.y));
+			PlayerHandler.movePlayer(player, new Vector2(-1, 0));
 
 		}
 		if (Gdx.input.isKeyPressed(Keys.S) && Gdx.input.isKeyPressed(Keys.A)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x - 1, player.position.y - 1));
+			PlayerHandler.movePlayer(player, new Vector2(-1, -1));
 		}
 		if (Gdx.input.isKeyPressed(Keys.S) && Gdx.input.isKeyPressed(Keys.D)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x + 1, player.position.y - 1));
+			PlayerHandler.movePlayer(player, new Vector2(1, -1));
 		}
 		if (Gdx.input.isKeyPressed(Keys.S)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x, player.position.y - 1));
+			PlayerHandler.movePlayer(player, new Vector2(0, -1));
 		}
 
 		if (Gdx.input.isKeyPressed(Keys.D)) {
-			PlayerHandler.movePlayer(player, new Vector2(player.position.x + 1, player.position.y));
+			PlayerHandler.movePlayer(player, new Vector2(1, 0));
 		}
 	}
 }
