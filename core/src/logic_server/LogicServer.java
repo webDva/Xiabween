01012725e05bbff1a5e7b6946e0427229206a40f
sorcerer <windows146@users.noreply.github.com>
2015-08-18 12:37:00 -0400

    Added LogicServer for data handling.

---
+++ core/src/logic_server/LogicServer.java
@@ -0,0 +1,14 @@
+package logic_server;
+
+public class LogicServer {
+
+	/*
+	 * For holding data such as a game class's speed, base damage stats, etc.
+	 */
+
+	public final float baseplayer_speed = 1;
+
+	public void getJSONData() { // For external databases.
+
+	}
+}
