faf9a297200b822c1656dbb62ae0dbb1e2dec16d
Easterling <windows146@users.noreply.github.com>
2015-09-04 03:34:02 -0400

    Added notice to Renderer head.

--- core/src/render/Renderer.java
+++ core/src/render/Renderer.java
@@ -1,7 +1,28 @@
+/*
+    Xiabween
+    Copyright (C) 2015  Easterling
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package render;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import backend.Logician;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,7 +31,6 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import backend.Logician;
 import entities.Fireball;
 import entities.Map_struct;
 import entities.PlayerEntity;
@@ -55,9 +75,8 @@ public class Renderer {
 
 		batch.begin();
 		elapsedTime += Gdx.graphics.getDeltaTime();
-		batch.draw(animation.getKeyFrame(elapsedTime, true), player.position.x, player.position.y,
-				animation.getKeyFrame(elapsedTime).getRegionWidth() * SPRITE_SCALING,
-				animation.getKeyFrame(elapsedTime).getRegionHeight() * SPRITE_SCALING);
+		batch.draw(animation.getKeyFrame(elapsedTime, true), player.position.x, player.position.y, animation.getKeyFrame(elapsedTime)
+				.getRegionWidth() * SPRITE_SCALING, animation.getKeyFrame(elapsedTime).getRegionHeight() * SPRITE_SCALING);
 		batch.end();
 
 		player.isAnimating = false;
