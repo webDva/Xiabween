73cc22a25c176d58bff5993eeedbe15412e29670
sorcerer <windows146@users.noreply.github.com>
2015-09-03 21:54:44 -0400

    Created start of willCollide function to check for collisions.

--- core/src/render/CollisionDetector.java
+++ core/src/render/CollisionDetector.java
@@ -1,11 +1,27 @@
 package render;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Handles collision detection.
- * 
+ *
  * @author sorcerer
  *
  */
 public class CollisionDetector {
+
+	/**
+	 * Determines if two objects will collide. The first is usually a moving
+	 * object while the second is stationary, like a tile.
+	 *
+	 * @param firstObjectCoordinates
+	 *            The moving object.
+	 * @param secondObjectCoordinates
+	 *            The stationary object.
+	 * @return true if the two objects will collide with each other.
+	 */
+	public static boolean willCollide(Vector2 firstObjectCoordinates, Vector2 secondObjectCoordinates) {
+		return false;
+	}
 
 }
