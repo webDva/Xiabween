87f8c1dfd055fb759b2174e90714ca30568c2a2c
sorcerer <windows146@users.noreply.github.com>
2015-09-10 18:04:26 -0400

    Changed XiaMessageRequest to an interface from a class.

--- core/src/logic_subsystem/XiaMessageRequest.java
+++ core/src/logic_subsystem/XiaMessageRequest.java
@@ -2,10 +2,10 @@ package logic_subsystem;
 
 /**
  * A data structure that is used in the Xiabween Messaging System.
- * 
+ *
  * @author sorcerer
  *
  */
-public class XiaMessageRequest {
+public interface XiaMessageRequest {
 
 }
