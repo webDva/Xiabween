ba7e199c53cdbbd26fa84b7887f5f5c9b954465d
sorcerer <windows146@users.noreply.github.com>
2015-09-10 01:22:23 -0400

    Protoyped first method for XID, determining how XID will behave.

--- core/src/logic_subsystem/XiabweenInternalDatabase.java
+++ core/src/logic_subsystem/XiabweenInternalDatabase.java
@@ -2,4 +2,8 @@ package logic_subsystem;
 
 public class XiabweenInternalDatabase extends XIDECSCComponent {
 
+	public void queryForData() { // TODO: Might want to make XID abstract too with abstract methods for encapsulating, because
+									// there might be more than one type of XID, e.g., for single player or multiplayer.
+	}
+
 }
