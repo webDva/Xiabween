eec0a83e362f7138754a1fcf37c57cfeb574eaef
Easterling <windows146@users.noreply.github.com>
2015-09-12 17:02:44 -0400

    Created RequestType enum.

--- core/src/logic_subsystem/XiaMessageRequest.java
+++ core/src/logic_subsystem/XiaMessageRequest.java
@@ -4,7 +4,9 @@ package logic_subsystem;
  * A data structure that is used in the Xiabween Messaging System.
  *
  * @author sorcerer
- *
  */
 public class XiaMessageRequest {
+
+	public RequestType RequestType;
+
 }
