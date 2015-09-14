2a5d3a18a6d2d87b52518dd63cb860983b8f0433
Easterling <windows146@users.noreply.github.com>
2015-09-13 21:52:25 -0400

    Created dictionary of XIDECSCComponents.

--- core/src/logic_subsystem/GameMaster.java
+++ core/src/logic_subsystem/GameMaster.java
@@ -1,5 +1,8 @@
 package logic_subsystem;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * Even though {@link XiabweenInternalDatabase} holds data, this
  * {@link GameMaster} class too will hold data, so it can't be abstract. After
@@ -11,13 +14,12 @@ package logic_subsystem;
  */
 public abstract class GameMaster {
 
-	// Should these three be in a list or class? If in a class, what would it look like and how to use it?
-	protected XiabweenInternalDatabase minionXID;
-	protected ExistenceCreator minionEC;
-	protected StateContainer minionSC;
+	protected Map<XIDECSCComponentType, XIDECSCComponent> components;
 
-	public GameMaster() {
-		// TODO Auto-generated constructor stub
+	public GameMaster(ArrayList<XIDECSCComponent> componentArray) {
+		for (XIDECSCComponent component : componentArray) {
+			this.components.put(component.type, component);
+		}
 	}
 
 }
