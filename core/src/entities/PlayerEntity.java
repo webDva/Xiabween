ac1020f522d21f7df9766028eb846ea8132e1698
Easterling <windows146@users.noreply.github.com>
2015-09-15 22:19:19 -0400

    Created PI radian constants.

--- core/src/entities/PlayerEntity.java
+++ core/src/entities/PlayerEntity.java
@@ -3,7 +3,7 @@ package entities;
 import com.badlogic.gdx.math.Rectangle;
 
 /**
- * A Xia struct for rendering a PlayerLogic object.
+ * A XiaEntity struct for rendering a PlayerLogic object.
  *
  * @author sorcerer
  *
@@ -11,9 +11,9 @@ import com.badlogic.gdx.math.Rectangle;
 public class PlayerEntity extends XiaEntity {
 
 	public static final String UP = "north", RIGHT = "east", DOWN = "south", LEFT = "west";
+	public static final double EAST = 0 * Math.PI, NORTH = Math.PI / 2, WEST = Math.PI, SOUTH = 3 * Math.PI / 2;
 
-	public String direction; // Should make this an enumeration with radians.
-								// Actually, the radians would be better suited for the game logic form of the player class.
+	public String direction;
 	public boolean isAnimating;
 	public PlayerTextures textures; // Contains idle and walking animation frames.
 	public String name;
