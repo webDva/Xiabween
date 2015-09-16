c9d787770dae93228d2174bd8081e30e41fb06ad
Easterling <windows146@users.noreply.github.com>
2015-09-15 22:58:47 -0400

    Created constructors for the Players.

--- core/src/player/HumanPlayer.java
+++ core/src/player/HumanPlayer.java
@@ -4,4 +4,13 @@ public class HumanPlayer extends ActualPlayerClass {
 
 	public final MobType mobType = MobType.HUMAN_PLAYER;
 
+	public HumanPlayer(float x, float y, float initialDirection, String name, String texturePath, StatsClass stats) {
+		this.x = x;
+		this.y = y;
+		this.radians_Direction = initialDirection;
+		this.playerName = name;
+		this.texturePath = texturePath;
+		this.stats = stats;
+	}
+
 }
--- core/src/player/MobPlayer.java
+++ core/src/player/MobPlayer.java
@@ -4,4 +4,13 @@ public class MobPlayer extends ActualPlayerClass {
 
 	public final MobType mobType = MobType.MOB_PLAYER;
 
+	public MobPlayer(float x, float y, float initialDirection, String name, String texturePath, StatsClass stats) {
+		this.x = x;
+		this.y = y;
+		this.radians_Direction = initialDirection;
+		this.playerName = name;
+		this.texturePath = texturePath;
+		this.stats = stats;
+	}
+
 }
