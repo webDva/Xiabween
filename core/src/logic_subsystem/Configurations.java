c66a18147a52a2e14668bc4be26ac42a88747d31
Easterling <windows146@users.noreply.github.com>
2015-09-17 19:00:29 -0400

    Created MOVE_UP command vector.

--- core/src/logic_subsystem/Configurations.java
+++ core/src/logic_subsystem/Configurations.java
@@ -1,9 +1,11 @@
 package logic_subsystem;
 
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector2;
 
 public class Configurations {
 
-	public static final int MOVE_UP = Keys.W;
+	public static final int MOVE_UP_KEY = Keys.W;
+	public static final Vector2 MOVE_UP = new Vector2(-1, 1);
 
 }
