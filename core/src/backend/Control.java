053b5e3d3cbd11acdf67edd1f7381c9f291a5f2e
Easterling <windows146@users.noreply.github.com>
2015-09-18 03:52:37 -0400

    Won't be using that checkIfMovementKeys method anymore.

--- core/src/backend/Control.java
+++ core/src/backend/Control.java
@@ -1,36 +1,9 @@
 package backend;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Vector2;
-
-import entities.PlayerEntity;
 
 public class Control {
 
 	public static final Vector2 MOVE_UP = new Vector2(-1, 1);
 
-	public static void checkIfMovementKeyIsPressed(PlayerEntity player) {
-		if (Gdx.input.isKeyPressed(Keys.W) && Gdx.input.isKeyPressed(Keys.A)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.LEFT, new Vector2(-1, 1));
-		} else if (Gdx.input.isKeyPressed(Keys.W) && Gdx.input.isKeyPressed(Keys.D)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.RIGHT, new Vector2(1, 1));
-		}
-
-		else if (Gdx.input.isKeyPressed(Keys.S) && Gdx.input.isKeyPressed(Keys.A)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.LEFT, new Vector2(-1, -1));
-		} else if (Gdx.input.isKeyPressed(Keys.S) && Gdx.input.isKeyPressed(Keys.D)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.RIGHT, new Vector2(1, -1));
-		}
-
-		else if (Gdx.input.isKeyPressed(Keys.W)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.UP, new Vector2(0, 1));
-		} else if (Gdx.input.isKeyPressed(Keys.A)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.LEFT, new Vector2(-1, 0));
-		} else if (Gdx.input.isKeyPressed(Keys.S)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.DOWN, new Vector2(0, -1));
-		} else if (Gdx.input.isKeyPressed(Keys.D)) {
-			PlayerEntityHandler.movePlayer(player, PlayerEntity.RIGHT, new Vector2(1, 0));
-		}
-	}
 }
